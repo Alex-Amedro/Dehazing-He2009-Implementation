8de5807c91e3f3315596c3aaa1b558b236c59240
aao5442 <aao5442@rocket.enseeiht.fr>
2026-05-05 09:48:16 +0200

    implementation soft matting

--- src/functions/soft_matting.py
+++ src/functions/soft_matting.py
@@ -1,24 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sp
+import os
 from transmission_estimation import transmission_estimation as te
-def soft_matting(I : np.ndarray, t : np.ndarray ) -> np.ndarray:
-    
+
+def soft_matting(I : np.ndarray, t_tild : np.ndarray ) -> np.ndarray:
+    # para
+    taille_fenetre = 3
+    norm_w = taille_fenetre*taille_fenetre
+    e = 0.0001
+    lambda_e = 0.0001
+    U_3 = np.identity(3)
+
     # Vectorisation
     H, W, C = I.shape
     N = H*W
-    e = 0.0001
     I_flat = I.reshape(N, C)
-    t_flat = t.flatten()
-    I_pad = np.pad(I,1,mode = "edge")
-    L = np.zeros((N,N))
-    U = np.identity(N)
-    for i in range(0,H) : 
-        for j in range(0,W) :
-            voisinage = I[max(0 ,i-1):min(H-1,i+2),max(0,j-1):min(W-1,j+2)]
+    t_tild_flat = t_tild.flatten()
+
+    U = sp.sparse.eye(N, format='csr')
+
+    #padding sur les cotés de l'image en copiant le 3 canaux couleurs
+    I_pad = np.pad(I, ((1, 1), (1, 1), (0, 0)), mode = "edge")
+    
+    # L
+    indices_lignes = []
+    indices_colonnes = []
+    valeurs = []
+
+    # parcours pixels
+    total_pixels = (H-3) * (W-3)
+    current_pixel = 0
+    for i in range(1,H-2) :
+        for j in range(1,W-2) :
+            current_pixel += 1
+            if current_pixel % max(1, total_pixels // 20) == 0:  # Afficher tous les 5%
+                progress = (current_pixel / total_pixels) * 100
+                print(f"Progression: {progress:.1f}%")
+            voisinage = I[i:i+taille_fenetre,j:j+taille_fenetre,:]
             moyenne_int = np.mean(voisinage, axis=0)
             moyenne_k = np.mean(moyenne_int, axis = 0)
             voinisage_flat = voisinage.reshape(-1,3)
             cov_k = np.cov(voinisage_flat, rowvar=False, ddof=0)
+            B = np.linalg.inv(cov_k+(e/9)*(U_3))
+
+            # parcours de couple dans la fenetres
             for l in range(9):
                 for k in range(9):
                     kron = 0
@@ -28,19 +54,47 @@ def soft_matting(I : np.ndarray, t : np.ndarray ) -> np.ndarray:
                     x_k = k % 3 - 1
                     y_k = k // 3 -1 
 
-                    idx_l = (i-x_l)*(j-y_l)
-                    idx_k = (i-x_l)*(j-y_l)
-                    if idx_l == idx_k :
+                    i_l = min(max(i + y_l, 0), H - 1)
+                    j_l = min(max(j + x_l, 0), W - 1)
+                    
+                    i_k = min(max(i + y_k, 0), H - 1)
+                    j_k = min(max(j + x_k, 0), W - 1)
+
+                    idx_l = i_l * W + j_l
+                    idx_k = i_k * W + j_k
+
+                    if l == k :
                         kron = 1
-                    A = np.transpose(I(idx_l)-moyenne_k)
-                    B = np.linalg.inv(cov_k+(e/9)*(U))
-                    C = I(idx_k)-moyenne_k
-                    L[idx_l][idx_k] += kron - ((1/9)*(1+np.matmul(np.matmul(A,B),C)))
 
+                    A = voinisage_flat[l]-moyenne_k
+                    C = voinisage_flat[k]-moyenne_k
 
+                    value_mat = np.dot(A,np.dot(B,C))
+                    value_L = kron - ((1/9)*(1+value_mat))
+                    indices_lignes.append(idx_l)
+                    indices_colonnes.append(idx_k)
+                    valeurs.append(value_L)
+
+    L = sp.sparse.coo_matrix((valeurs, (indices_lignes, indices_colonnes)), shape=(N, N)).tocsr()
+    A = L + lambda_e * U
+    b = lambda_e * t_tild_flat
+    t_flat, _ = sp.sparse.linalg.cg(A, b)
+    t = t_flat.reshape(H, W)
+
+    return t
 
 if __name__ == "__main__":
-    img = plt.imread("../../data/raw_images/image1.jpg")
-    transminsion = te(img, 50)
-    soft_matting(img,transminsion)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    img_path = os.path.join(current_dir, "../../data/raw_images/image2.jpeg")
+    img = plt.imread(img_path)
+    transmission_bloc= te(img, 50)
+    transmission = soft_matting(img,transmission_bloc)
+    fig = plt.figure()
+    original = fig.add_subplot(1, 2, 1)
+    original.imshow(img)
+    trs_tild = fig.add_subplot(1, 2, 2)
+    trs_tild.imshow(transmission_bloc, cmap='gray')
+    trs = fig.add_subplot(1, 2, 3)
+    trs.imshow(transmission, cmap='gray')
+    plt.show()
     
